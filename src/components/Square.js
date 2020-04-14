b09bf488f9c852ecf121290f4f6859a5af6653a3
povilasurbonas <povilas.urbonas85@gmail.com>
2020-04-14 20:01:45 +0200

    using  classnames dependency

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,22 @@
 import React from 'react';
+const classNames = require('classnames');
 
 export default function Square(props) {
-  let subclass;
   const { value, handleClick, text, isGameover } = props;
-  text.props.children == 'X' && (subclass = 'square--human');
-  text.props.children == 'O' && (subclass = 'square--computer');
-  let classname = `square ${subclass}`;
+  let btnClass = classNames('square', {
+    'square--human': text.props.children == 'X',
+    'square--computer': text.props.children == 'O',
+  });
+  // let subclass;
+  //
+  // text.props.children == 'X' && (subclass = 'square--human');
+  // text.props.children == 'O' && (subclass = 'square--computer');
+  // let classname = `square ${subclass}`;
 
   return (
     <span>
       <button
-        className={classname}
+        className={btnClass}
         disabled={text.props.children || isGameover}
         value={value}
         onClick={handleClick}
